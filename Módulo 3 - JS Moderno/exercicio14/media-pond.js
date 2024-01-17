2f64d507d5e337d5de5c7552653358b8d3b24b62
Igor Santos <031igorsantos@gmail.com>
2024-01-16 21:21:54 -0300

    main

--- Módulo 3 - JS Moderno/exercicio14/media-pond.js
+++ Módulo 3 - JS Moderno/exercicio14/media-pond.js
@@ -1,28 +1,19 @@
+const weightedArithmeticMean = (...numbers) => {
+  return (
+    numbers
+      .map((number) => number.n * number.w)
+      .reduce((initial, next) => initial + next) /
+    numbers.map((number) => number.w).reduce((initial, next) => initial + next)
+  );
+};
+
 document.querySelector("#media-pond").addEventListener("click", () => {
   const numbers = [
     { n: 7, w: 2 },
     { n: 9, w: 5 },
     { n: 3, w: 1 },
   ];
-  const multiNumWei = numberMultipledWeight(...numbers);
-  const sumW = sumWeight(...numbers);
-  const media = mediaAritPond(multiNumWei, sumW);
-  console.log(media.toFixed(1));
-});
 
-const numberMultipledWeight = (...numbers) => {
-  let numMultiWeight = 0;
-  numbers.forEach((number) => {
-    numMultiWeight += number.n * number.w;
-  });
-  return numMultiWeight;
-};
-const sumWeight = (...numbers) => {
-  let sumWeight = 0;
-  numbers.forEach((number) => (sumWeight += number.w));
-  return sumWeight;
-};
-const mediaAritPond = (multiNumWei, sumWeight) => {
-  const media = multiNumWei / sumWeight;
-  return media;
-};
+  console.clear();
+  console.log(weightedArithmeticMean(...numbers));
+});
