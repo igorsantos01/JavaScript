39ec04b39c79f16bda3dbaed2987f8c254ec1bb4
Igor Santos <031igorsantos@gmail.com>
2024-01-17 21:25:03 -0300

    main

--- Módulo 3 - JS Moderno/exercicio14/mediana.js
+++ Módulo 3 - JS Moderno/exercicio14/mediana.js
@@ -1,6 +1,15 @@
-const numbers = [2, 4, 5, 7, 42, 99, 5];
+const median = (...numbers) => {
+  numbers.sort((a, b) => a - b);
+  const middle = Math.floor(numbers.length / 2);
+  if (numbers.length % 2 !== 0) {
+    return numbers[middle];
+  } else {
+    const firstMedian = numbers[middle - 1];
+    const lastMedian = numbers[middle];
+    return (firstMedian + lastMedian) / 2;
+  }
+};
 document.querySelector("#mediana").addEventListener("click", () => {
-  const index = parseInt(numbers.length / 2);
-
-  console.log(index);
+  const numbers = [2, 4, 5, 7, 42, 99, 6];
+  console.log(median(...numbers), numbers);
 });
